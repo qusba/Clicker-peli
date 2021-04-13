9e415a1d4b83a1f3914156b055be21ca56de8bad
qusba <kasper.kahkonen@outlook.com>
2021-04-13 22:39:29 +0300

    .

---
+++ src/tests/game_logic_test.py
@@ -0,0 +1,12 @@
+import unittest
+from game_logic import GameLogic
+
+
+class TestGameLogic(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_clicking_produces_score(self):
+        self.logic = GameLogic()
+        self.logic.click()
+        self.assertEqual(self.logic.score,1)
