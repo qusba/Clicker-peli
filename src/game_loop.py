6507198d96f60ef130843f7375e3d5dee664512a
qusba <kasper.kahkonen@outlook.com>
2021-04-15 15:06:35 +0300

    pylint muokkauksia

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,14 +1,13 @@
+from sys import exit
 import pygame
-
 class GameLoop():
     def __init__(self,gamelogic,display):
         self.display = display
         self.gamelogic = gamelogic
         self.clock = pygame.time.Clock()
-        
-
+        self.font = pygame.font.SysFont("Arial", 24)
+        self.text = self.font.render("Score: "+str(self.gamelogic.score),True,(255,0,0))
     def start(self):
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -16,15 +15,9 @@ class GameLoop():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.gamelogic.click()
-
-            
-            font = pygame.font.SysFont("Arial", 24)
-            self.text = font.render("Score: "+str(self.gamelogic.score),True,(255,0,0))
-
+            self.text = self.font.render("Score: "+str(self.gamelogic.score),True,(255,0,0))
             self.display.fill((0,0,0))
             self.display.blit(self.text, (10,10))
- 
             pygame.display.flip()
             self.clock.tick(60)
-
-
+            
--- src/index.py
+++ src/index.py
@@ -1,29 +1,14 @@
 import pygame
 from game_logic import GameLogic
 from game_loop import GameLoop
-
-
-
-
-
 def main():
-
     display = pygame.display.set_mode((1024,640))
     pygame.display.set_caption("Clicker")
     pygame.font.init()
-   
-
     logic = GameLogic()
     game_loop = GameLoop(logic,display)
-
-
     pygame.init()
     game_loop.start()
-
-
 if __name__ == "__main__":
     main()
-    
-
-
     
